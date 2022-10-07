d8a788f359fa73951bab6953e9aa32ce401f0f2a
ilhamadi77 <ilham.adi78@gmail.com>
2022-10-07 11:03:05 +0700

    hello GO lang

---
+++ helloword.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+
+func main(){
+	fmt.Println("hello ini ilham");
+	fmt.Println("satu", 1);
+	fmt.Println("stau koma dua",1.2);
+	fmt.Println("benar", false);
+	fmt.Println("salah", true);
+	fmt.Println("jumlah nya = ", len("juml"));
+	fmt.Println("jumlah nya string = ", "juml"[2] );
+
+}
---
+++ variable.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Ilham Adi irawan"
+	fmt.Println(name)
+
+	name = "Adi"
+	fmt.Println(name)
+
+	benar := true
+	fmt.Println(benar)
+
+	var (
+		firstname = "ilham"
+		lastname  = "Irawan"
+		age       = 70
+		life      = true
+		sick      = false
+	)
+
+	fmt.Println(firstname)
+	fmt.Println(lastname)
+	fmt.Println(age)
+	fmt.Println(life)
+	fmt.Println(sick)
+
+}
